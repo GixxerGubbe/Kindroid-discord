2351290c23af57bba77d4d93d766cef70edc8206
GixxerGubbe <botisering@gmail.com>
2026-03-08 22:18:48 +0100

    Remove unused 'node-cron' import
    Removed the import statement for 'node-cron'.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import { initializeAllBots, shutdownAllBots } from "./discordManager";
 import { BotConfig } from "./types";
-import cron from 'node-cron';
 
 dotenv.config();
 
